cb06f564a92c31f6c46b9c6cfeffc22bdd5bdc36
kevalrudakiya <kevalrudakiya7@gmail.com>
2026-09-21 20:59:55 +0530

    Add files via upload

---
+++ # Printing text and numbers.py
@@ -0,0 +1,12 @@
+# Printing text and numbers
+print("Hello, World!")
+print(42)
+
+# Assigning variables
+user_name = "Alex"
+age = 21
+height = 5.9
+is_student = True
+
+# Formatted string (f-string) to combine variables with text
+print(f"Name: {user_name}, Age: {age}, Height: {height}ft, Active: {is_student}")
